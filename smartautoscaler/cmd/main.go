ae478a7783fa4b9c8f28e017ec9a7cce98d66aad
kudmo <kurko.dm@gmail.com>
2026-02-09 14:09:38 +0300

    Набросок на сбор метрик из prometheus. Не проверено с работающим окружением.

--- smartautoscaler/cmd/main.go
+++ smartautoscaler/cmd/main.go
@@ -1,5 +1,52 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+)
+
+// Config содержит конфигурацию сборщика
+type Config struct {
+	PrometheusURL string
+	Interval      time.Duration
+	Queries       []MetricQuery
+}
+
+// MetricQuery описывает один запрос к Prometheus
+type MetricQuery struct {
+	Name  string
+	Query string
+	Help  string
+}
+
+// MetricCollector основной сборщик метрик
+type MetricCollector struct {
+	api    v1.API
+	config Config
+	logger *log.Logger
+}
+
+// NewMetricCollector создает новый сборщик метрик
+func NewMetricCollector(config Config) (*MetricCollector, error) {
+	client, err := api.NewClient(api.Config{
+		Address: config.PrometheusURL,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("ошибка создания клиента: %w", err)
+	}
+
+	return &MetricCollector{
+		api:    v1.NewAPI(client),
+		config: config,
+		logger: log.New(os.Stdout, "", log.LstdFlags),
+	}, nil
+}
 
 func main() {
-
+	fmt.Println("Hello METANIT.COM!")
 }
--- smartautoscaler/go.mod
+++ smartautoscaler/go.mod
@@ -1,3 +1,14 @@
 module github.com/kudmo/CoolPA
 
 go 1.21
+
+require github.com/prometheus/client_golang v1.21.0
+
+require (
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	google.golang.org/protobuf v1.36.1 // indirect
+)
