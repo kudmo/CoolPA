28d58db89da9c626076f456c96dfe626a1fdb82c
kudmo <kurko.dm@gmail.com>
2026-02-26 17:48:07 +0300

    Исправлен сбор метрик: теперь будут собираться метрики только для наших приложений, помеченных autoscaling-enabled, причём без вспомогательных контейнеров на подах

--- smartautoscaler/cmd/main.go
+++ smartautoscaler/cmd/main.go
@@ -20,73 +20,175 @@ func main() {
 		Queries: []collector.MetricQuery{
 			{
 				Name:  "kube_pod_info",
-				Query: `kube_pod_info{namespace="autoscale-test"}`,
+				Query: `kube_pod_info * on (namespace, pod) group_left() (kube_pod_labels{label_autoscaling_enabled="true"})`,
 				Help:  "Kubernetes pod metadata",
 			},
 			{
-				Name:  "container_cpu_usage",
-				Query: `sum by (namespace,pod,container) (rate(container_cpu_usage_seconds_total{namespace="autoscale-test", container!=""}[1m]))`,
-				Help:  "CPU usage per container (cores)",
+				Name: "container_cpu_usage",
+				Query: `sum by (namespace, pod, container) (
+					rate(container_cpu_usage_seconds_total{container!="",container!="istio-proxy",container!="POD"}[1m])
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "CPU usage per container (cores)",
 			},
 			{
-				Name:  "container_memory_usage",
-				Query: `sum by (namespace,pod,container) (container_memory_usage_bytes{namespace="autoscale-test", container!=""})`,
-				Help:  "Memory usage per container (bytes)",
+				Name: "container_memory_usage",
+				Query: `sum by (namespace, pod, container) (
+					container_memory_usage_bytes{container!="",container!="istio-proxy",container!="POD"}
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "Memory usage per container (bytes)",
 			},
 			{
-				Name:  "container_cpu_quota",
-				Query: `sum by (namespace,pod,container) (container_spec_cpu_quota{namespace="autoscale-test", container!=""})`,
-				Help:  "CPU quota per container",
+				Name: "container_cpu_quota",
+				Query: `sum by (namespace, pod, container) (
+					container_spec_cpu_quota{container!="",container!="istio-proxy",container!="POD"}
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "CPU quota per container",
 			},
 			{
-				Name:  "container_memory_limit",
-				Query: `sum by (namespace,pod,container) (container_spec_memory_limit_bytes{namespace="autoscale-test", container!=""})`,
-				Help:  "Memory limit per container (bytes)",
+				Name: "container_memory_limit",
+				Query: `sum by (namespace, pod, container) (
+					container_spec_memory_limit_bytes{container!="",container!="istio-proxy",container!="POD"}
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "Memory limit per container (bytes)",
 			},
 			{
-				Name:  "container_fs_usage",
-				Query: `sum by (namespace,pod,container) (container_fs_usage_bytes{namespace="autoscale-test", container!=""})`,
-				Help:  "Filesystem usage per container (bytes)",
+				Name: "container_fs_usage",
+				Query: `sum by (namespace, pod, container) (
+					container_fs_usage_bytes{container!="",container!="istio-proxy",container!="POD"}
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "Filesystem usage per container (bytes)",
 			},
 			{
-				Name:  "container_fs_write",
-				Query: `sum by (namespace,pod,container) (rate(container_fs_writes_bytes_total{namespace="autoscale-test", container!=""}[1m]))`,
-				Help:  "Filesystem write time per container",
+				Name: "container_fs_write",
+				Query: `sum by (namespace, pod, container) (
+					rate(container_fs_writes_bytes_total{container!="",container!="istio-proxy",container!="POD"}[1m])
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "Filesystem write bytes per container",
 			},
 			{
-				Name:  "container_fs_read",
-				Query: `sum by (namespace,pod,container) (rate(container_fs_reads_bytes_total{namespace="autoscale-test"}[1m]))`,
-				Help:  "Filesystem read time per container",
+				Name: "container_fs_read",
+				Query: `sum by (namespace, pod, container) (
+					rate(container_fs_reads_bytes_total{container!="",container!="istio-proxy",container!="POD"}[1m])
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "Filesystem read bytes per container",
 			},
 			{
-				Name:  "container_network_receive",
-				Query: `sum by (namespace,pod) (rate(container_network_receive_bytes_total{namespace="autoscale-test"}[1m]))`,
-				Help:  "Network receive bytes per pod",
+				Name: "container_network_receive",
+				Query: `sum by (namespace, pod) (
+					rate(container_network_receive_bytes_total{}[1m])
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "Network receive bytes per pod",
 			},
 			{
-				Name:  "container_network_transmit",
-				Query: `sum by (namespace,pod) (rate(container_network_transmit_bytes_total{namespace="autoscale-test"}[1m]))`,
-				Help:  "Network transmit bytes per pod",
+				Name: "container_network_transmit",
+				Query: `sum by (namespace, pod) (
+					rate(container_network_transmit_bytes_total{}[1m])
+					* on (namespace, pod) group_left()
+					(kube_pod_labels{label_autoscaling_enabled="true"})
+				)`,
+				Help: "Network transmit bytes per pod",
 			},
 			{
-				Name:  "istio_request_duration_p95",
-				Query: `histogram_quantile(0.95, sum (rate(istio_request_duration_milliseconds_bucket{namespace="autoscale-test", destination_workload!=""}[1m])) by (le, destination_workload))`,
-				Help:  "Istio P95 request latency per workload",
+				Name: "istio_request_duration_p95",
+				Query: `histogram_quantile(0.95, 
+						sum by (le, destination_app, source_app) (rate(istio_request_duration_milliseconds_bucket{destination_workload!=""}[1m])) 
+					) 
+					* on (source_app) group_left()
+					label_replace(
+						kube_pod_labels{label_autoscaling_enabled="true"},
+						"source_app",
+						"$1",
+						"label_app",
+						"(.*)"
+					)
+					* on (destination_app) group_left()
+					label_replace(
+						kube_pod_labels{label_autoscaling_enabled="true"},
+						"destination_app",
+						"$1",
+						"label_app",
+						"(.*)"
+					)`,
+				Help: "Istio P95 request latency per app (both source and destination must have autoscaling-enabled)",
 			},
 			{
-				Name:  "istio_requests_total",
-				Query: `sum(rate(istio_requests_total{destination_workload!="", namespace="autoscale-test", reporter="destination"}[1m])) by (destination_workload, source_workload)`,
-				Help:  "Istio HTTP requests per workload (RPS)",
+				Name: "istio_requests_total",
+				Query: `sum by (destination_app, source_app) (rate(istio_requests_total{destination_workload!="", reporter="destination"}[1m])) 
+					* on (source_app) group_left()
+					label_replace(
+						kube_pod_labels{label_autoscaling_enabled="true"},
+						"source_app",
+						"$1",
+						"label_app",
+						"(.*)"
+					)
+					* on (destination_app) group_left()
+					label_replace(
+						kube_pod_labels{label_autoscaling_enabled="true"},
+						"destination_app",
+						"$1",
+						"label_app",
+						"(.*)"
+					)`,
+				Help: "Istio HTTP requests per app (RPS) - both source and destination must have autoscaling-enabled",
 			},
 			{
-				Name:  "istio_tcp_received_bytes_total",
-				Query: `sum(rate(istio_tcp_received_bytes_total{namespace="autoscale-test", destination_workload!=""}[1m])) by (source_workload, destination_workload)`,
-				Help:  "Istio TCP received bytes per workload",
+				Name: "istio_tcp_received_bytes_total",
+				Query: `sum by (source_app, destination_app) (rate(istio_tcp_received_bytes_total{destination_workload!=""}[1m])) 
+					* on (source_app) group_left()
+					label_replace(
+						kube_pod_labels{label_autoscaling_enabled="true"},
+						"source_app",
+						"$1",
+						"label_app",
+						"(.*)"
+					)
+					* on (destination_app) group_left()
+					label_replace(
+						kube_pod_labels{label_autoscaling_enabled="true"},
+						"destination_app",
+						"$1",
+						"label_app",
+						"(.*)"
+					)`,
+				Help: "Istio TCP received bytes per app - both source and destination must have autoscaling-enabled",
 			},
 			{
-				Name:  "istio_tcp_sent_bytes_total",
-				Query: `sum(rate(istio_tcp_sent_bytes_total{namespace="autoscale-test", destination_workload!=""}[1m])) by (source_workload, destination_workload)`,
-				Help:  "Istio TCP sent bytes per workload",
+				Name: "istio_tcp_sent_bytes_total",
+				Query: `sum by (source_app, destination_app) (rate(istio_tcp_sent_bytes_total{destination_workload!=""}[1m])) 
+					* on (source_app) group_left()
+					label_replace(
+						kube_pod_labels{label_autoscaling_enabled="true"},
+						"source_app",
+						"$1",
+						"label_app",
+						"(.*)"
+					)
+					* on (destination_app) group_left()
+					label_replace(
+						kube_pod_labels{label_autoscaling_enabled="true"},
+						"destination_app",
+						"$1",
+						"label_app",
+						"(.*)"
+					)`,
+				Help: "Istio TCP sent bytes per app - both source and destination must have autoscaling-enabled",
 			},
 		},
 	}
