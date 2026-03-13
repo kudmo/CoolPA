6dc126c04249e577dbb210041c57b1672b39cb1f
kudmo <kurko.dm@gmail.com>
2026-03-13 14:19:06 +0300

    Код на GO работает. Но требуется обучить модель на разных SLO

--- smartautoscaler/decision/slo_predictor/predictor.go
+++ smartautoscaler/decision/slo_predictor/predictor.go
@@ -1,13 +1,11 @@
 package slopredictor
 
 import (
-	"fmt"
 	"log"
 
 	ort "github.com/yalue/onnxruntime_go"
 )
 
-// DummyPredictor is a simple rule-based predictor used as a placeholder.
 type RFPredictor struct{}
 
 func (d *RFPredictor) PredictBatch(X [][]float64) []float64 {
@@ -21,44 +19,35 @@ func (d *RFPredictor) PredictBatch(X [][]float64) []float64 {
 	}
 	defer ort.DestroyEnvironment()
 
-	// --- 3. Подготовьте входные и выходные тензоры ---
-	// Данные для предсказания. Должны быть в формате []float32.
-	// Здесь для примера массив из 20 признаков.
 	for i, x := range X {
 
 		inputData := x
 
 		inputShape := ort.NewShape(1, int64(len(inputData)))
-		inputTensor, err := ort.NewTensor(inputShape, inputData)
+
+		inputTensor, err := ort.NewTensor[float64](inputShape, inputData)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer inputTensor.Destroy()
 
-		// Определите форму выходного тензора.
-		// Для классификации это может быть (1, количество_классов) для вероятностей или (1,) для метки.
-		// Пока мы не знаем точный размер, можно создать пустой тензор с предположительной формой.
-		// Важно: если форма не совпадет с тем, что вернет модель, Run() вернет ошибку.
-		// Лучше всего заранее узнать форму выхода, например, из Python.
-		outputShape := ort.NewShape(1)
+		outputShape := ort.NewShape(1, 2)
+
 		outputTensor, err := ort.NewEmptyTensor[float32](outputShape)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer outputTensor.Destroy()
 
-		// --- 4. Создайте сессию для инференса ---
-		// Здесь мы используем AdvancedSession.
-		// Первый аргумент — путь к .onnx файлу.
-		// Второй — список имен входных слоев (их можно узнать из Python, например, sess.get_inputs()[0].name)
-		// Третий — список имен выходных слоев.
-		// Четвертый и пятый — списки входных и выходных тензоров.
-		session, err := ort.NewAdvancedSession("/app/random_forest_model.onnx",
-			[]string{"float_input"},
-			[]string{"output_label"},
-			[]ort.Value{inputTensor},
-			[]ort.Value{outputTensor},
-			nil)
+		session, err := ort.NewAdvancedSession(
+			"random_forest_model.onnx",
+			[]string{"double_input"},
+			[]string{"probabilities"},
+			[]ort.ArbitraryTensor{inputTensor},
+			[]ort.ArbitraryTensor{outputTensor},
+			nil,
+		)
+
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -70,8 +59,7 @@ func (d *RFPredictor) PredictBatch(X [][]float64) []float64 {
 		}
 
 		outputData := outputTensor.GetData()
-		fmt.Println("Предсказание модели в Go:", outputData)
-		out[i] = float64(outputData[0])
+		out[i] = float64(outputData[1]) // Вероятность попадения в класс 1 == нарушение SLO
 	}
 
 	return out
