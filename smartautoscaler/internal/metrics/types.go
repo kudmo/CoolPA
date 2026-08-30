bbcb1ee9371b07bd25facc614c028326344797e5
kudmo <kurko.dm@gmail.com>
2026-08-30 14:59:24 +0300

    Documentation of contract interfaces

--- smartautoscaler/internal/metrics/types.go
+++ smartautoscaler/internal/metrics/types.go
@@ -1,7 +1,20 @@
 package metrics
 
+// ServiceInfo contains descriptive information about a service
+// that is relevant for autoscaling decisions. It includes the
+// service's name and the names of services it communicates with
+// (inbound and outbound calls), which can be used to build a
+// service dependency graph.
 type ServiceInfo struct {
-	Name         string   `json:"name"`
+	// Name is the unique identifier of the service within the
+	// monitored namespace or cluster.
+	Name string `json:"name"`
+
+	// InboundCalls lists the names of services that call this service.
+	// These are incoming dependencies.
 	InboundCalls []string `json:"inbound_calls"`
+
+	// OuboundCalls lists the names of services that this service calls.
+	// These are outgoing dependencies.
 	OuboundCalls []string `json:"outbond_calls"`
 }
